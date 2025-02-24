919d2d501e0e8468252365b8da1e61263260dc8b
rasunM <rasunmanohara@gmail.com>
2025-02-24 22:55:52 +0530

    Added the firebase authentication

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ import {name as appName} from './app.json';
 import Question1 from './src/question-1';
 import UseMemoQuestion from './src/useMemo/use_memo';
 import UseContextPractice from './src/useContext';
+import RegisterPage from './src/auth';
 
-AppRegistry.registerComponent(appName, () => UseContextPractice);
+AppRegistry.registerComponent(appName, () => RegisterPage);
